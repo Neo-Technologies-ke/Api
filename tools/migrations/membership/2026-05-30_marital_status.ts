f46e4b092032b7be543da36e049cc7fc572d5ae0
Kevin Ithagu <ithagusk@gmail.com>
2026-05-30 21:23:18 +0300

    migration fixes

--- tools/migrations/membership/2026-05-30_marital_status.ts
+++ tools/migrations/membership/2026-05-30_marital_status.ts
@@ -1,15 +1,15 @@
-import { type Kysely } from "kysely";
+import { sql, type Kysely } from "kysely";
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable("people")
-    .alterColumn("maritalStatus", (col) => col.setDataType('varchar(20)'))
+    .modifyColumn("maritalStatus", sql`varchar(20)`)
     .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable("people")
-    .alterColumn("maritalStatus", (col) => col.setDataType(`varchar(10)`))
+    .modifyColumn("maritalStatus", sql`varchar(10)`)
     .execute();
 }
